356c2aa1243e532b67b50af8980443b3a79d718b
Matheus Vilela <matheus_vilela@icloud.com>
2020-08-16 08:04:22 -0300

    allowed_hosts

--- projetoFinal/gestaoclientes/gestaoclientes/settings.py
+++ projetoFinal/gestaoclientes/gestaoclientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [m-gestaoclientes.herokuapp.com]
+ALLOWED_HOSTS = ['m-gestaoclientes.herokuapp.com']
 
 
 # Application definition
