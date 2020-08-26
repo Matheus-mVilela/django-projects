215f4324b2caffebcfb6f0bdb8f59dd6edd7a83f
Matheus Vilela <matheus_vilela@icloud.com>
2020-08-26 19:49:04 -0300

    Primeiros passos Docker

--- projetoFinal/gestaoclientes/gestaoclientes/settings.py
+++ projetoFinal/gestaoclientes/gestaoclientes/settings.py
@@ -78,15 +78,10 @@ WSGI_APPLICATION = 'gestaoclientes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gestaoclientes',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
